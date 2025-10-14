c40803b33b19e8ac772b843996cfafcd95a22a51
Parantack Das <dasparantack@gmail.com>
2025-10-14 13:09:26 +0530

    Update Signup.jsx

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -17,7 +17,7 @@ export default function Signup() {
       return;
     }
     try {
-      const res = await fetch("http://localhost:5000/api/users/register", {
+      const res = await fetch("https://trading-app-backend-6ibt.onrender.com/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
